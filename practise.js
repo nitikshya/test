53ece46a2905f72796c5281e8dbff82d739ea9fc
Nitikshya Thapa <thapanitikshya@gmail.com>
2023-08-13 21:08:27 +0545

    day 1

---
+++ practise.js
@@ -0,0 +1,57 @@
+/*console.log('HELLO WORLD')
+console.log('This is Nitikshya Thapa')
+// variable
+address = 'From Biratnagar'
+console.log(address)
+
+//operator
+food = 500
+discountpercent = 0.5
+discountamount = food * discountpercent
+console.log(discountamount)*/
+
+//user input
+/*food = Number(prompt('how much for food?'))
+vatpercent = Number(prompt('input%') / 100)
+vatamount = food * vatpercent
+console.log('vatamount',vatamount)
+//alert(vatamount)
+total = food + vatamount
+console.log('total',total)*/
+
+// simplest weather app
+// conditionals
+/*let weather = prompt('how is the weather')
+if(weather=="rainy"){
+  console.log("get your raincoat")}
+else{
+  console.log('get your sunglasses')}*/
+
+//function
+/*function SayMyName(){
+    console.log('nanu')
+  }
+  SayMyName()*/
+  
+  //argument
+ /* function compliment(name){
+    comp= `Hi ${name}, you look amazing.`
+    console.log(comp)
+  }
+  compliment('Aakriti')
+  food=Number(prompt("Price of food?"));*/
+
+  //console.log(food)
+ /* discountPercentage=Number(prompt("Discount Percent?"));
+  discountAmount=food*(discountPercentage/100);
+  totalAmount=food-discountAmount;
+  alert(totalAmount) */
+
+
+  
+  food=Number(prompt("Price of food?"));
+//console.log(food)
+discountPercentage=Number(prompt("Discount Percent?"));
+discountAmount=food*(discountPercentage/100);
+totalAmount=food-discountAmount;
+alert(totalAmount) 
