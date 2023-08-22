a6eb9ac6031f0456a375616636866eed4b55cfeb
Nitikshya Thapa <thapanitikshya@gmail.com>
2023-08-22 22:40:19 +0545

    on no

--- api/script.js
+++ api/script.js
@@ -1,14 +1,34 @@
 // https://dog.ceo/api/breeds/image/random
 
 //Asynchronous Programming
-const dogimageDiv=document.getElementById('dogimage')
-const dog=document.getElementById('dog')
-const click=()=>{
- fetch('https://dog.ceo/api/breeds/image/random')
- .then(response=>response.json())
- .then(json=>{
-    dogimageDiv.innerHTML=`<img src='${json.message}'height=250 width=250/>`
+// const dogimageDiv=document.getElementById('dogimage')
+// const dog=document.getElementById('dog')
+// const click=()=>{
+//  fetch('https://dog.ceo/api/breeds/image/random')
+//  .then(response=>response.json())
+//  .then(json=>{
+//     dogimageDiv.innerHTML=`<img src='${json.message}'height=250 width=250/>`
+//    })
+// }
+// dog.onclick=()=> click()
+//heroimageDiv.innerHTML=`<img src='${json.message}' height=250,width=250/>`
+
+
+//superheroes
+const heroimage=document.getElementById('heroimage')
+const hero=document.getElementById('hero')
+
+const superherotoken='1793509237732044'
+const baseurl=`https://superheroapi.com/api.php/${superherotoken}`
+let comic=(id,name)=>{
+   fetch(`${baseurl}/${id}`)
+   .then(response=>response.json())
+   .then(json=>{
+      //console.log(json)
+      document.querySelector('body').innerHTML+= `<img src="${json.image.url}" height=250 width=250 />`
    })
 }
-dog.onclick=()=> click()
-
+let randomnumber= () =>{
+   return Math.floor(Math.random*731)
+}
+hero.onclick= ()=>comic(randomnumber())
