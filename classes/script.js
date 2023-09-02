2ae73aa022b1c1e6646f37ba57fc399de3ade657
Nitikshya Thapa <thapanitikshya@gmail.com>
2023-09-02 11:32:09 +0545

    day-9

--- classes/script.js
+++ classes/script.js
@@ -1,8 +1,13 @@
 
+const depositButton=document.getElementById('deposit')
+const withdrawButton=document.getElementById('withdraw')
+const amountInput=document.getElementById('amount')
+const balanceDiv=document.getElementById('balance')
  class money{
     constructor(balance){
         this.balance=balance
         console.log({previousbalance:`$${this.balance}`})
+        balanceDiv.innerText=`Balance:${balance}`
     }
     deposit(amount){
         this.balance+=Number(amount)
@@ -11,28 +16,38 @@
     }
     withdraw(amount){
         //guard clause
-        if (this.balance- amount<=0){
+        if (this.balance- amount<0){
             console.log("you can't withdraw money")
+            // balanceDiv.innerText=`You can't withdraw money`
             return
         }
         this.balance-=Number(amount)
         console.log('withdrew:',amount)
         console.log({balance:`$${this.balance}`})
+        balanceDiv.innerText=`Balance:${balance}`
     }
  }
  const bank= new money(100)
 
-  bank.deposit(100)
-  bank.withdraw(100)
-  //bank.withdrawal(100)
-  //bank.withdrawal(100)
-  bank.withdraw(500)
+//   bank.deposit(100)
+//   bank.withdraw(100)
+//   //bank.withdrawal(100)
+//   //bank.withdrawal(100)
+//   bank.withdraw(500)
 
-  const depositButton=document.getElementById('deposit')
-  const withdrawButton=document.getElementById('withdraw')
-  const amountInput=document.getElementById('amount')
-  depositButton.onclick=()=>console.log(amountInput.value)
-  withdrawButton.onclick=()=>console.log(amountInput.value)
+//   
+
+  //const balanceDiv=document.getElementById('balance')
+  depositButton.onclick=()=>{
+    //const bal=amountInput.value
+    bank.deposit(amountInput.value)
+    balanceDiv.innerText=`Balance:${bank.balance}`
+  }
+  withdrawButton.onclick=()=>{
+    //const bal=amountInput.value
+    bank.withdraw(amountInput.value)
+    balanceDiv.innerText=`Balance:${bank.balance}`
+  }
 
 
 
